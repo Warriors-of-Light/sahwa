746a2dfdd45fe138c994eb09d0c8f0fa8f342665
belassiria aymane <aymanebel2@outlook.fr>
2024-06-18 19:04:00 +0100

    fix avatar bullet

--- src/components/coursCard.tsx
+++ src/components/coursCard.tsx
@@ -5,7 +5,7 @@ import FlagAR from "@/assets/flag_ar.svg";
 import FlagEN from "@/assets/flag_en.svg";
 import Image from "next/image";
 import { FaRegStar } from "react-icons/fa";
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 
 export const CoursCard:FC<{data: coursDetails}> = ({data})=>{
@@ -46,10 +46,7 @@ export const CoursCard:FC<{data: coursDetails}> = ({data})=>{
             </div>
             <div className="flex flex-row-reverse">
                 <div className="flex -space-x-1 overflow-hidden">
-                    <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-                    <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-                    <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt=""/>
-                    <img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                    {Array.from(Array(4)).map(()=><img className="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" key={uuidv4()}/>)}
                 </div>
                 <p>٢٠٠+ ملتحق</p>
             </div>
