e96e236469e824e4d71481d7f6e34e4a488ac1e8
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2024-07-27 11:03:20 +0200

    [Feature] link profile with backend

--- src/models/interfaces/tag.ts
+++ src/models/interfaces/tag.ts
@@ -1,16 +1,4 @@
 export interface Tag {
-  message: string | null
-  status: number | null
-  id: number 
-  name: string 
-}
-
-export interface TagsResponse {
-  message: string
-  status: number
-  page_number: number | null
-  page_size: number | null
-  total_pages: number | null
-  total_records: number | null
-  list: Tag[]
+    id: number;
+    name: string;
 }
